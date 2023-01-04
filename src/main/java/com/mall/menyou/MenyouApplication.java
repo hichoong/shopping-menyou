d46d5cbc8fc1a7bd4904afe0488c779f21bf1377
choong <ksky9230@naver.com>
2023-01-04 18:43:03 +0900

    프로젝트 생성

---
+++ src/main/java/com/mall/menyou/MenyouApplication.java
@@ -0,0 +1,13 @@
+package com.mall.menyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenyouApplication.class, args);
+	}
+
+}
